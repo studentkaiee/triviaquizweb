2287ab3019bf1edd48b7caeeec304f84fe86efe8
Zacharykoh <2101952F@student.tp.edu.sg>
2023-01-14 23:28:53 +0800

    Development[db integration]: Integrated database to login and register.
    ~ edited LoginServlet.java to enable login function to work with
    database values.
    ~ edited RegisterServlet.java to enable register function to work with
    database values.
    
    Reviewed-by: Zachary

--- src/main/java/com/dvops/maven/eclipse/LoginServlet.java
+++ src/main/java/com/dvops/maven/eclipse/LoginServlet.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * Servlet implementation class LoginServlet
@@ -35,11 +40,40 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter print = response.getWriter();
-		print.println("<script type=\"text/javascript\">");
-		print.println("alert('You have successfully login!');"); // alert message
-		print.println("location='index.jsp';"); // redirect to home page
-		print.println("</script>");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(
+			"jdbc:mysql://localhost:3306/user", "root", "password");
+			
+			Statement  stm = con.createStatement();
+			ResultSet rs = stm.executeQuery("select * from user where username='"+username+"' and password='"+password+"'");
+			if(rs.next()) {
+				PrintWriter print = response.getWriter();
+				print.println("<script type=\"text/javascript\">");
+				print.println("alert('You have successfully login!');"); // alert message
+				print.println("location='index.jsp';"); // redirect to home page
+				print.println("</script>");
+				
+			}else {
+				
+				PrintWriter print = response.getWriter();
+				print.println("<script type=\"text/javascript\">");
+				print.println("alert('Invalid Credentials!');"); // alert message
+				print.println("location='login.jsp';");
+				print.println("</script>");
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+
 	}
 
 }
